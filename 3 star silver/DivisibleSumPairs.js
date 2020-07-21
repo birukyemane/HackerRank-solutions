c44c9302c14f0921547224fe2161efdf0b831a14
birukyemane <bir_yem@yahoo.com>
2020-07-21 20:44:24 +0300

    - define DivisibleSumPairs

---
+++ 3 star silver/DivisibleSumPairs.js
@@ -0,0 +1,18 @@
+https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?h_r=next-challenge&h_v=zen
+// Easy
+// Max Score 10
+
+function divisibleSumPairs(n, k, ar) {
+    let noOfPairs = 0;
+
+    for(let i=0;i <= ar.length-2;i++){
+        for(let j=i+1;j <= ar.length-1;j++){
+            console.log(i,j)
+            if((ar[i]+ar[j])%k === 0) {
+                console.log('Inside the if')
+                noOfPairs = noOfPairs +1;
+            }
+        }
+    }
+    return noOfPairs;
+}
